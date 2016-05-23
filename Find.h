8998a3eaad20c8f5e25334479b07a39cd04cb2ea
HandsomeBoby <769374355@qq.com>
2016-05-23 23:23:47 +0800

    二维数组的查找

--- Find.h
+++ Find.h
@@ -2,7 +2,7 @@
 
 bool Find(vector<vector<int> > array, int target)
 {
-	int row = array.size();//行数
+	int row = array.size();//
 	int col = array[0].size();//列数
 	int m = 0;
 	int n = col - 1;//第一次选取右上角的元素
