b0db52153027ab7ab51e4e597c212f5a5cb162d1
HandsomeBoby <769374355@qq.com>
2016-07-21 22:51:31 +0800

    判断是否是二叉搜索树的后序遍历结果

---
+++ VerifySquenceOfBST.h
@@ -0,0 +1,25 @@
+class Solution {
+public:
+	bool VerifySquenceOfBST(vector<int> sequence) {
+		int size = sequence.size();
+		if (size == 0)
+		{
+			return false;
+		}
+
+		int i = 0;
+		while (--size)
+		{
+			while (sequence[i++]<sequence[size]);
+			while (sequence[i++]>sequence[size]);
+
+			if (i<size)
+			{
+				return false;
+			}
+			i = 0;
+		}
+		return true;
+
+	}
+};
